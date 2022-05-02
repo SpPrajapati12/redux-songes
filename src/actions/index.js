d344d5779b992107b8b094a1136a806fdd93225d
SpPrajapati12 <101313637+SpPrajapati12@users.noreply.github.com>
2022-05-02 16:17:26 +0530

    commit

---
+++ src/actions/index.js
@@ -0,0 +1,9 @@
+//Action
+const selectSong = (song) => {
+    return {
+        type: "SONG_SELECTED",
+        payload: song
+    }
+
+}
+export default selectSong;
---
+++ src/components/SongList.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {connect} from 'react-redux'
+import selectSong from "../actions"
+
+
+const SongList = (props) => {
+
+  const renderList = () => {
+        return props.songs.map((song) => {
+            return (
+                <div className='item' key={song.title}>
+                    <div className="right floated content">
+                        <button className="ui button primary" onClick={() => props.selectSong(song)}>Select</button>
+                    </div>
+                    <div className="content">
+                        {song.title}
+                        <div>
+                            <b>{song.title === props.favoriteTitle && 'Favorite!'}</b>
+                        </div>
+                    </div>
+                </div>
+            );
+        });
+    }
+
+
+    return (  
+        <div className='ui divided list'>{renderList()}</div>
+    );
+}
+
+
+const mapStateToProps = (state) => {
+    console.log(state)
+    return {songs : state.songs}
+
+}
+ 
+export default connect(mapStateToProps,{
+    selectSong : selectSong
+    // selectSong 
+})(SongList);
